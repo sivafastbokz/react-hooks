0ef61f256d73ec5640fff0703f2a3e6628339d7d
sivafastbokz <sivaharshanfastbokz@gmail.com>
2023-09-02 17:51:03 +0530

    practiced

--- src/conponent/useRef.js
+++ src/conponent/useRef.js
@@ -1,12 +1,30 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 function UserData(){
-    const[userName,setUserName]=useState('');
+    const[count,setCount]=useState(0);
+    const counts = useRef(0); 
+    const inputTag = useRef()
+    const add = ()=>{
+        setCount(count +1)
+    }
 
+    const inputFocus = ()=>{
+        inputTag.current.focus()
+    }
+    const handleAdd =()=>{
+       counts.current++
+       console.log(`button clicked ${counts.current} times`)
+    }
     return(
         <>
-        <input type='text' onChange={(event)=>setUserName(event.target.value)}></input>
-        <h2>{userName}</h2>
+        {console.log('rendered')}
+         <button onClick={add}>add useState</button>
+         <div>{count}</div>
+         <br/>
+         <button onClick={handleAdd}>add useRef</button>
+         <br/>
+         <input type='text' ref={inputTag}></input>
+         <button onClick={inputFocus}>focus</button>
         </>
     )
 }
